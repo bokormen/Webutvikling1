2d126722ecce38dc1c626fef9f63977c4e3e0e7d
froste76 <frode.steinnes@gmail.com>
2015-04-29 10:54:21 +0200

    Lagt til login-side og mer innhold på ordresiden med tilhørende css og scripts

---
+++ javascript/length.js
@@ -0,0 +1,20 @@
+// Method to populate an array with numbers
+Array.prototype.populate = function(n) {
+	return Object.keys(Object(0+Array(n+1)));
+};
+
+// Creates an array filled with numbers from 1-28
+var weeks = [].populate(4);
+
+// Creates an element for each number in the array
+var elements = document.getElementById("howManyWeeks");
+var i = 1; // The lowest number possible to choose from the pull down menu
+// This for-loop starts with the number chosen above and populates the html select tag
+// with the number of option fields ending with the last number in the created array (days[]).
+for (i; i < weeks.length; i++) {
+	var opt = weeks[i];
+	var el = document.createElement("option");
+	el.textContent = opt;
+	el.value = opt;
+	elements.appendChild(el);
+}
